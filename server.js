0baf0decd948ee0e84a68df3e66a2ba19ab9819d
rafaelmendonca29 <mendonca.rafael.araujo@gmail.com>
2021-09-16 16:54:47 -0300

    Conexão com o BD, configuração do Mongoose e envio de mensagens

--- server.js
+++ server.js
@@ -1,3 +1,50 @@
+require('dotenv').config()
+
 var express = require('express')
-var app = express();
-var server= app.listen(3000,()=>{console.log('server is running on port ', server.address().port)})
+var mongoose = require('mongoose')
+var bodyParser = require('body-parser')
+
+var app = express()
+
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+
+//declaração da url de conexão com o banco de dados utilizando variáveis de ambiente
+var dbUrl = 'mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASSWORD+'@'+process.env.DB_CLUSTER +'/'+process.env.DB_NAME +'?retryWrites=true&w=majority'
+
+//Conexão com o banco de dados
+mongoose.connect(dbUrl,(err)=> {
+  console.log('mongodb connected', err)
+});
+
+//Definição do Modelo da Mensagem
+var Message = mongoose.model('Message', 
+                                      {name: String,
+                                       message:String
+                                      })
+
+//rotas
+
+//capturando mensagens do usuário no BD
+app.get('/messages',(req,res)=>{
+  Message.find({},(err,messages)=>{
+    res.send(messages);
+  })
+})
+
+//enviando mensagens do usuário para o BD
+
+app.post('/messages',(req,res)=>{
+  var message = new Message(req.body);
+  message.save((err)=>{
+    if(err)
+      sendStatus(500);
+    res.sendStatus(200);
+  })  
+})
+
+
+
+var server= app.listen(3000, () => {console.log('server is running on port ', server.address().port)})
