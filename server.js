85330ff307212bcfb8893d5308ad565642b4d479
rafaelmendonca29 <mendonca.rafael.araujo@gmail.com>
2021-09-16 17:56:04 -0300

    Configuração do Socket.io que permite atualização em tempo real

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 require('dotenv').config()
-
 var express = require('express')
 var mongoose = require('mongoose')
 var bodyParser = require('body-parser')
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
 var app = express()
 
@@ -25,7 +26,7 @@ var Message = mongoose.model('Message',
                                        message:String
                                       })
 
-//rotas
+//ROTAS
 
 //capturando mensagens do usuário no BD
 app.get('/messages',(req,res)=>{
@@ -41,10 +42,14 @@ app.post('/messages',(req,res)=>{
   message.save((err)=>{
     if(err)
       sendStatus(500);
+    io.emit('message',req.body);
     res.sendStatus(200);
   })  
 })
 
-
+//Configuração do Socket i.o
+io.on('connection',()=>{
+  console.log('a user is connected')
+})
 
 var server= app.listen(3000, () => {console.log('server is running on port ', server.address().port)})
