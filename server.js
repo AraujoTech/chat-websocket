adb7a025867acba07418dc51c9ac955e866e9d64
rafaelmendonca29 <mendonca.rafael.araujo@gmail.com>
2021-09-23 15:30:06 -0300

    Estado anterior

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-const requirejs = require('requirejs');
-const env = require('dotenv').config();
+//require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
